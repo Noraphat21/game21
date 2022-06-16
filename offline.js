71f18fedc825cabd799d001ebf371c7bc9636ad9
Noraphat21 <107599790+Noraphat21@users.noreply.github.com>
2022-06-16 13:16:37 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1655360077,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/รถ-sheet0.png",
+		"images/ต้นไม้-sheet0.png",
+		"images/ดาว-sheet0.png",
+		"images/ต้นไม้2-sheet0.png",
+		"images/ระเบิด-sheet0.png",
+		"images/it-sheet0.png",
+		"images/รถ2-sheet0.png",
+		"images/it2-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
